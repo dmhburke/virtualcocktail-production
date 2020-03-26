f5a99fcebae32e93176630f2027e4840fcc1e365
Daniel Burke <dmhburke@gmail.com>
2020-03-26 14:52:29 -0400

    test remove default_file_storage

--- virtualCocktailApp/settings.py
+++ virtualCocktailApp/settings.py
@@ -143,8 +143,8 @@ AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-DEFAULT_FILE_STORAGE = 'virtualCocktailApp.storage_backends.MediaStorage'
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# DEFAULT_FILE_STORAGE = 'virtualCocktailApp.storage_backends.s3boto3.S3Boto3Storage' ##DB REMOVED: NOT SURE THIS IS NEEDED
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'catalog/static'),
 ]
