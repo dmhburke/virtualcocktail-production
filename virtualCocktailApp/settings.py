b0b9fb7e3716e64211788e4de5c72d481d1b1121
Daniel Burke <dmhburke@gmail.com>
2020-03-26 15:04:05 -0400

    adjust file structure

--- virtualCocktailApp/settings.py
+++ virtualCocktailApp/settings.py
@@ -143,7 +143,7 @@ AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-# DEFAULT_FILE_STORAGE = 'virtualCocktailApp.storage_backends.s3boto3.S3Boto3Storage' ##DB REMOVED: NOT SURE THIS IS NEEDED
+DEFAULT_FILE_STORAGE = 'virtualCocktailApp.storage_backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'catalog/static'),
