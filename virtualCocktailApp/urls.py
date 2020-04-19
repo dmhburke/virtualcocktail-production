ce17f9ec1783486ba2fab1a821982b81ce713630
Daniel Burke <dmhburke@gmail.com>
2020-04-18 21:21:52 -0400

    revert to pre-PWA

--- virtualCocktailApp/urls.py
+++ virtualCocktailApp/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
     path('', RedirectView.as_view(url='', permanent=True)),
-]
-
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
